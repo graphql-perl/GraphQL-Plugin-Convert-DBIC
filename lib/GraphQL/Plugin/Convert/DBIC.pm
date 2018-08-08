4dee5e21dacc87166ceffcd3b55c5ca168131c57
Ed J <mohawk2@users.noreply.github.com>
2018-08-08 05:32:20 +0100

    v0.07

--- lib/GraphQL/Plugin/Convert/DBIC.pm
+++ lib/GraphQL/Plugin/Convert/DBIC.pm
@@ -6,7 +6,7 @@ use GraphQL::Schema;
 use GraphQL::Debug qw(_debug);
 use Lingua::EN::Inflect::Number qw(to_S);
 
-our $VERSION = "0.06";
+our $VERSION = "0.07";
 use constant DEBUG => $ENV{GRAPHQL_DEBUG};
 
 my %GRAPHQL_TYPE2SQLS = (
